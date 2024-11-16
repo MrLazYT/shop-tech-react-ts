37d3a4d92021589d0cdbd06d456ad4c70c2e7f04
TSShrol <shrolts@gmail.com>
2024-11-16 19:01:03 +0200

    added MUI: Card , Grid v2

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,11 @@
 import { IProductDto } from "../types/product";
-
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 // export default function ProductCard({ id, title, description, price, imagePath, categoryId, categoryName }: IProductDto) {
 //     return (
 //         <div> 
@@ -12,11 +18,28 @@ import { IProductDto } from "../types/product";
 
 const ProductCard:React.FC<IProductDto>=({ id, title, description, price, imagePath, categoryId, categoryName })=> {
     return (
-        <div> 
-            <h2>{title}</h2>
-            <p>{price} грн.</p>
-        </div>
-
-    )
+        <Card sx={{ height: '100%' }}>
+        <CardMedia
+          sx={{ height: 160, backgroundSize: 'contain'}}
+          image={imagePath}
+          title={title}
+        />
+        <CardContent>
+          <Typography gutterBottom fontSize={20} component="div">
+            {title}
+          </Typography>
+          <Typography gutterBottom  fontSize={18} variant="h6" component="div">
+            {price} грн.
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {description}
+          </Typography>
+          </CardContent>
+        <CardActions>
+          <Button size="small">Details</Button>
+          <Button size="small">Add to Cart</Button>
+        </CardActions>
+      </Card>
+   )
 }
 export default ProductCard;
--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,35 +1,59 @@
 import { useEffect, useState } from "react";
 import { IProductDto } from "../types/product";
 import ProductCard from "./ProductCard";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#fff',
+    ...theme.typography.body2,
+    fontSize: 23,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    ...theme.applyStyles('dark', {
+        backgroundColor: '#1A2027',
+    }),
+}));
+
+
+
 
 const api: string = "https://webapi-shoptech-pv321-hxc4cna7gab9ghhc.westeurope-01.azurewebsites.net/api/products";
-const ProductsList:React.FC = () => {
+const ProductsList: React.FC = () => {
+
     const [products, setProducts] = useState<IProductDto[] | undefined>();
-    useEffect(()=>{
+    useEffect(() => {
         fetch(api)
-            .then(res=>res.json())
-            .then(data=>{
+            .then(res => res.json())
+            .then(data => {
                 console.log(data);
                 setProducts(data);
                 // console.log(products);
-            }).catch(err=>console.error(err));
+            }).catch(err => console.error(err));
 
-    },[]);
+    }, []);
 
     return (
-        <div>
+        <>
             <h1>List products</h1>
-            <ul>
-                {
-                    products ? (
-                        products?.map((product)=>
-                        <li key={product.id}> <ProductCard {...product}/>  </li>
-                    )):
-                        <p>Downloading....</p>
-                }
-            </ul>
-
-        </div>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={4}>
+                        {
+                            products ? (
+                                products?.map((product) =>
+                                <Grid key={product.id} size={{ xs: 12, sm:6, md: 3 }}>
+                                     <ProductCard {...product} /> 
+                                </Grid>
+                                )) :
+                                <p>Downloading....</p>
+                        }
+                </Grid>
+            </Box>
+        </>
     );
 }
 
