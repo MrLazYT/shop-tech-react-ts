4f83d072a1eb4c2e7c18b28cc82aa0337219582b
TSShrol <shrolts@gmail.com>
2024-11-18 20:02:08 +0200

    modefied baseUrl

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { IProductDto } from "../types/product";
 import { getTokenFromLocalStorage } from "../helper/localStorage.helper";
-// const urlAPI='https://webapi-shoptech-pv321-hxc4cna7gab9ghhc.westeurope-01.azurewebsites.net/api/';
-const urlAPI='https://localhost:7262/api/';
+const urlAPI='https://webapi-shoptech-pv321-hxc4cna7gab9ghhc.westeurope-01.azurewebsites.net/api/';
+// const urlAPI='https://localhost:7262/api/';
 
 export const instance = axios.create({
     baseURL: urlAPI,
